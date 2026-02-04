d402a94007ab01e38d6e3a22f80184711c7cbb19
musa <musa.hstu11@gmail.com>
2026-02-04 20:32:31 +0600

    created a simple api that returns error rate

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+type ErrorResponse struct {
+	ErrorRate float32 `json:"error_rate"`
+	Message   string  `json:"message"`
+}
+
+func getErrorRate(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+
+	response := ErrorResponse{
+		ErrorRate: 20.5,
+		Message:   "Current error rate",
+	}
+
+	encoder := json.NewEncoder(w)
+	err := encoder.Encode(response)
+	log.Println(r.Method, r.URL.Path, time.Since(start))
+	if err != nil {
+		http.Error(w, "Failed to process response", http.StatusInternalServerError)
+		return
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/apis/error", http.HandlerFunc(getErrorRate))
+	port := ":8080"
+	err := http.ListenAndServe(port, mux)
+	if err != nil {
+		fmt.Println("Something went wrong. Failed to start the server")
+	}
+}
